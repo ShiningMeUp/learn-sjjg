027eb06e9f12d2c4bdd9d5261303148004a97744
Shining <L1930911375@gmail.com>
2024-05-20 16:29:58 +0800

    141

--- LeetCode/141链表有无环(快慢指针法).cpp
+++ LeetCode/141链表有无环(快慢指针法).cpp
@@ -3,7 +3,8 @@
         if(head==nullptr)
         return false;
         ListNode *slow=head,*fast=head;
-        while(fast!=nullptr && fast->next != nullptr)
+        while(fast!=nullptr && fast->next != nullptr)  
+		/*没有fast->next != nullptr时，若链表为[1]时，fast和slow都为nullptr，当两个指针都为nullptr是，是相等的*/ 
         {
             slow=slow->next;//慢指针每次向后移动1个 
             fast=fast->next->next;//快指针每次向后移动2个 
