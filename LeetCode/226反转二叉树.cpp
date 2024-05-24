f4c431e8ace59b48949e42896ef68669a6589e85
Shining <L1930911375@gmail.com>
2024-05-24 20:07:39 +0800

    反转二叉树

---
+++ LeetCode/226反转二叉树.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == nullptr) return root;
+        TreeNode *temp;
+            temp=root->left;
+            root->left=root->right;
+            root->right=temp;
+            root->left=invertTree(root->left);//ตÝน้ 
+            root->right=invertTree(root->right);//ตÝน้ 
+            return root;
+    }
+};
